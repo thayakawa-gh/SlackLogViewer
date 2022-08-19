416417c6506350863f752177752b1c5edf805701
cielavenir <cielartisan@gmail.com>
2022-08-20 02:42:13 +0900

    moved apple deployqt definition

--- SlackLogViewer/DeployQt.cmake
+++ SlackLogViewer/DeployQt.cmake
@@ -17,6 +17,12 @@ if(WIN32)
                                   --$<IF:$<CONFIG:Debug>,debug,release> --qmldir ${CMAKE_SOURCE_DIR} 
                                   --no-quick-import --no-translations --no-system-d3d-compiler --no-compiler-runtime 
                                   --no-webkit2 --no-angle --no-opengl-sw)")
+elseif(APPLE)
+    find_program(DEPLOYQT NAMES macdeployqt HINTS "${_qt_bin_path}")
+    add_custom_command(
+        TARGET ${PROJECT_NAME} POST_BUILD
+        COMMAND ${DEPLOYQT} ${CMAKE_BINARY_DIR}/${PROJ_NAME}.app $<IF:$<CONFIG:Debug>,-use-debug-lib,>)
+    install(CODE "execute_process(COMMAND ${DEPLOYQT} ${CMAKE_BINARY_DIR}/${PROJ_NAME}.app $<IF:$<CONFIG:Debug>,-use-debug-lib,>")
 endif()
 
 
